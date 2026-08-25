e1efc1134c8b66006eeede7e6b5033c03f32dc60
anujaraired <anuj@yourdomain.com>
2026-08-26 00:27:48 +0530

    fix build

--- src/app/(website)/contact/page.tsx
+++ src/app/(website)/contact/page.tsx
@@ -1,4 +1,4 @@
-// import Heading from "@/src/components/common/Heading";
+import Heading from "@/src/components/common/Heading";
 import ContactForm from "@/src/components/form/ContactForm";
 import MaxWidth from "@/src/components/layout/MaxWidth";
 import { staticData } from "@/src/utills/Data";
@@ -6,11 +6,11 @@ import Image from "next/image";
 import React from "react";
 
 const Page = () => {
-  const { label, heading, description, data } = staticData?.contact;
+  const { label, headingParts, description, data } = staticData?.contact;
   return (
     <MaxWidth className="py-16 grid grid-cols-1 lg:grid-cols-2 gap-[1rem] lg:gap-[4rem]">
       <div className="my-auto">
-        {/* <Heading heading={heading} label={label} description={description} /> */}
+        <Heading headingParts={headingParts} label={label} description={description} />
         <div className="pt-[2rem]">
           {data?.map((item, idx: number) => {
             return (
