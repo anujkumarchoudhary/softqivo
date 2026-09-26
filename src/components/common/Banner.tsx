2c39cee5bd3508f407aa947248c67ad2d7e5d937
anujaraired <anuj@yourdomain.com>
2026-09-26 13:25:22 +0530

    finxed issue

--- src/components/common/Banner.tsx
+++ src/components/common/Banner.tsx
@@ -55,7 +55,7 @@ const Banner = () => {
       className="py-12 lg:py-20 bg-primary-bg  w-full overflow-hidden"
     >
       {/* Purple localized glow + grid */}
-      <div className="hidden md:block pointer-events-none absolute -left-24 -top-24 h-[420px] w-[520px] lg:h-[420px] lg:w-[520px] opacity-70">
+      <div className="pointer-events-none absolute -left-24 -top-24 h-[220px] w-[220px] lg:h-[420px] lg:w-[520px] opacity-70">
         <div className="absolute inset-0 rounded-full bg-purple-600/[0.12] blur-[120px]" />
 
         <div
@@ -74,7 +74,7 @@ const Banner = () => {
         />
       </div>
       {/* Blue localized glow + grid */}
-      <div className="hidden md:block pointer-events-none absolute -bottom-28 right-0 h-[220px] w-[220px] lg:h-[420px] lg:w-[520px] opacity-70">
+      <div className="pointer-events-none absolute -bottom-28 right-0 h-[220px] w-[220px] lg:h-[420px] lg:w-[520px] opacity-70">
         <div className="absolute inset-0 rounded-full bg-blue-600/[0.12] blur-[120px]" />
 
         <div
